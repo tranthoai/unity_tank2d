59b28fd068cae1acb7920a88b5651f925bdba307
Thoai Tran <Thoai.Tran>
2022-10-25 01:01:40 +0700

    Init

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D ab;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ab = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
